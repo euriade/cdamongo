a372db7b05c70948f6c396b83415360378c2e08f
euriade <ljessicalejeune@gmail.com>
2024-02-15 10:19:23 +0100

    cars controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+
+    constructor(private readonly carService: CarsService) {}
+
+    @Post()
+    async create(@Body() createCarDto: CreateCarDto) {
+        await this.carService.create(createCarDto);
+    }
+}
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Car } from './schema/car.schema';
+import { Model } from 'mongoose';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+
+    constructor(@InjectModel(Car.name) private readonly Model<Car>) {}
+
+    async create(createCarDto: CreateCarDto): Promise<Car> {
+        const createdCar = await this.carModel.create(createCarDto);
+        return createdCar;
+    }
+}
